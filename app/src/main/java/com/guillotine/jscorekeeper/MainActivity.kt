8ce032673482366cd14e5a0c60c9a5e495d0f456
Trevor Martin <tcmartin5000@gmail.com>
2024-10-07 02:23:02 -0400

    Placeholder nav graph implemented.

--- app/src/main/java/com/guillotine/jscorekeeper/MainActivity.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/MainActivity.kt
@@ -11,7 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.guillotine.jscorekeeper.composable.MenuScreenComposable
 import com.guillotine.jscorekeeper.ui.theme.JScorekeeperTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +24,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JScorekeeperTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = MenuScreen
+                ) {
+                    composable<MenuScreen> {
+                        MenuScreenComposable(navController, applicationContext)
+                    }
+                    composable<GameScreen> {
+                        Text("Game Screen!")
+                    }
+                    composable<PastGamesListScreen> {
+                        Text("History Screen!")
+                    }
                 }
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JScorekeeperTheme {
-        Greeting("Android")
-    }
-}
+// Basic architecture:
+// Screens for menu, game, stats, past games.
+// Game should be self-contained, "back" should save state and return to menu.
+// Game should remove itself from the back stack when finished.
+// Game should be flexible, capable of running the three different English-language formats.
