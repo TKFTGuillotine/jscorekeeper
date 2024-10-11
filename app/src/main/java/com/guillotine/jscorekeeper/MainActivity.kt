74f74ee0a46dc063c390f0736ebaad9b6c5b1c35
Trevor Martin <tcmartin5000@gmail.com>
2024-10-11 04:06:48 -0400

    Cleaned up imports.

--- app/src/main/java/com/guillotine/jscorekeeper/MainActivity.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/MainActivity.kt
@@ -4,21 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import com.guillotine.jscorekeeper.composable.GameScreenComposable
-import com.guillotine.jscorekeeper.composable.MenuScreenComposable
+import com.guillotine.jscorekeeper.composable.game.GameScreenComposable
+import com.guillotine.jscorekeeper.composable.menu.MenuScreenComposable
 import com.guillotine.jscorekeeper.ui.theme.JScorekeeperTheme
-import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
