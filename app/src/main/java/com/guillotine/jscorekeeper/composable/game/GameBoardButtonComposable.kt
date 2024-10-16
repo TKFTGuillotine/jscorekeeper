9ba29302dd56c3faf375e8c4c95e71a1f28e9633
Trevor Martin <tcmartin5000@gmail.com>
2024-10-16 02:05:47 -0400

    Reference local font instead of passing args.

--- app/src/main/java/com/guillotine/jscorekeeper/composable/game/GameBoardButtonComposable.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/composable/game/GameBoardButtonComposable.kt
@@ -8,20 +8,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.guillotine.jscorekeeper.R
 
 @Composable
 fun GameBoardButton(
     text: String,
-    fontFamily: FontFamily,
     fontSize: TextUnit = 50.sp,
     modifier: Modifier? = null,
     onClick: () -> Unit
 ) {
+    // This turns out to create much less headache on many fronts than async fonts.
+    val bebasNeueFamily = FontFamily(Font(R.font.bebas_neue))
+
     Button(
         colors = ButtonDefaults.buttonColors(
             containerColor = Color(0xFF00178E),
@@ -33,7 +37,7 @@ fun GameBoardButton(
     ) {
         Text(
             text = text,
-            fontFamily = fontFamily,
+            fontFamily = bebasNeueFamily,
             fontSize = fontSize,
             textAlign = TextAlign.Center
         )
