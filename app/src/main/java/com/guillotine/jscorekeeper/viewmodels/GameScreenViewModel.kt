250c17bb105a88e36b6a6a58cb0e7c660881ed7d
Trevor Martin <tcmartin5000@gmail.com>
2024-10-17 00:35:34 -0400

    Added functions/values to handle Daily Doubles.

--- app/src/main/java/com/guillotine/jscorekeeper/viewmodels/GameScreenViewModel.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/viewmodels/GameScreenViewModel.kt
@@ -1,26 +1,22 @@
 package com.guillotine.jscorekeeper.viewmodels
 
-import android.app.Application
-import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
-import com.guillotine.jscorekeeper.GameData
-import com.guillotine.jscorekeeper.GameModes
-import com.guillotine.jscorekeeper.R
+import com.guillotine.jscorekeeper.data.ClueDialogState
+import com.guillotine.jscorekeeper.data.GameData
 
-class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean): ViewModel() {
+class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel() {
     var score by mutableStateOf(0)
         private set
     var round by mutableStateOf(0)
         private set
     var moneyValues by mutableStateOf(gameData.moneyValues)
         private set
-    var isShowRoundDialog by mutableStateOf(false)
+    var clueDialogState by mutableStateOf(ClueDialogState.NONE)
         private set
-    var isShowClueDialog by mutableStateOf(false)
+    var isShowRoundDialog by mutableStateOf(false)
         private set
     var currentValue by mutableStateOf(0)
         private set
@@ -29,8 +25,25 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean): ViewModel(
     val rounds = gameData.rounds
     private val columns = gameData.columns
 
-    fun changeScore(delta: Int) {
-        score += delta
+    fun dailyDoubleScoreChange(wager: Int) {
+        score += wager
+        clueDialogState = ClueDialogState.NONE
+    }
+
+    fun isWagerValid(wager: Int): Boolean {
+        if (wager > score && wager > moneyValues.max()) {
+            return false
+        } else if (wager < 5) {
+            return false
+        } else {
+            clueDialogState = ClueDialogState.DAILY_DOUBLE_RESPONSE
+            currentValue = wager
+            return true
+        }
+    }
+
+    fun isRemainingDailyDouble(): Boolean {
+        return true
     }
 
     fun showRoundDialog() {
@@ -39,7 +52,7 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean): ViewModel(
 
     fun showClueDialog(value: Int) {
         currentValue = value
-        isShowClueDialog = true
+        clueDialogState = ClueDialogState.MAIN
     }
 
     fun onRoundDialogDismiss() {
@@ -47,7 +60,7 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean): ViewModel(
     }
 
     fun onClueDialogDismiss() {
-        isShowClueDialog = false
+        clueDialogState = ClueDialogState.NONE
     }
 
     fun isDoubleJ(): Boolean {
@@ -63,27 +76,27 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean): ViewModel(
         // Rounds is number of rounds. Round is index starting at 0. Second to last round is Double.
         if (round == rounds - 2) {
             // map returns a List rather than an IntArray. Must convert.
-            moneyValues = moneyValues.map{it * 2}.toIntArray()
+            moneyValues = moneyValues.map { it * 2 }.toIntArray()
         }
         isShowRoundDialog = false
     }
 
-    fun correctResponse(value: Int) {
+    fun onCorrectResponse(value: Int) {
         score += value
-        isShowClueDialog = false
+        onClueDialogDismiss()
     }
 
-    fun incorrectResponse(value: Int) {
+    fun onIncorrectResponse(value: Int) {
         score -= value
-        isShowClueDialog = false
+        onClueDialogDismiss()
     }
 
-    fun noAnswer(value: Int) {
-        isShowClueDialog = false
+    fun onPass(value: Int) {
+        onClueDialogDismiss()
     }
 
-    fun dailyDouble(value: Int) {
-        isShowClueDialog = false
+    fun onDailyDouble() {
+        clueDialogState = ClueDialogState.DAILY_DOUBLE_WAGER
     }
 
 }
