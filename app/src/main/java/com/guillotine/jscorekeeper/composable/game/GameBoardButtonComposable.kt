14992976d0a6de51dc7e2f83af09bdbe9573cebc
Trevor Martin <tcmartin5000@gmail.com>
2024-10-17 00:36:58 -0400

    Refactored for new theming.

--- app/src/main/java/com/guillotine/jscorekeeper/composable/game/GameBoardButtonComposable.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/composable/game/GameBoardButtonComposable.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.guillotine.jscorekeeper.R
 import com.guillotine.jscorekeeper.ui.theme.ClueButtonTheme
-import com.guillotine.jscorekeeper.ui.theme.ClueCardTheme
 
 @Composable
 fun GameBoardButton(
