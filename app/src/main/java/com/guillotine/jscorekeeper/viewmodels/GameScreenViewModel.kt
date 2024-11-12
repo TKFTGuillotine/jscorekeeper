8ec78d4c7ead42b0b4a3b4abb6fd7fe41b63ecd6
Trevor Martin <tcmartin5000@gmail.com>
2024-11-12 00:29:57 -0500

    Added/modified logic for tracking remaining clues by value, Daily Doubles.

--- app/src/main/java/com/guillotine/jscorekeeper/viewmodels/GameScreenViewModel.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/viewmodels/GameScreenViewModel.kt
@@ -1,5 +1,6 @@
 package com.guillotine.jscorekeeper.viewmodels
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -20,16 +21,23 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel
         private set
     var currentValue by mutableStateOf(0)
         private set
+    var snackbarHostState by mutableStateOf(SnackbarHostState())
+        private set
 
     val currency = gameData.currency
     private val multipliers = gameData.multipliers
     private val baseMoneyValues = gameData.moneyValues
     private val columns = gameData.columns
 
-    fun dailyDoubleScoreChange(wager: Int) {
-        score += wager
-        clueDialogState = ClueDialogState.NONE
-    }
+    // Initializes a map inline where each key is associated with columns.
+    private var columnsPerValue = moneyValues.associateWith { columns - 1 }.toMutableMap()
+
+    // State for delayed deduction of Daily Double value
+    private var isDailyDouble = false
+    private var dailyDoubleInitialValue = 0
+
+    // Initialized to first value, will increment later.
+    private var remainingDailyDoubles = multipliers[0]
 
     fun isWagerValid(wager: Int): Boolean {
         if (wager > score && wager > moneyValues.max()) {
@@ -44,7 +52,11 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel
     }
 
     fun isRemainingDailyDouble(): Boolean {
-        return true
+        return remainingDailyDoubles != 0
+    }
+
+    fun isValueRemaining(value: Int): Boolean {
+        return columnsPerValue[value] != 0
     }
 
     fun showRoundDialog() {
@@ -52,8 +64,12 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel
     }
 
     fun showClueDialog(value: Int) {
-        currentValue = value
-        clueDialogState = ClueDialogState.MAIN
+        if (columnsPerValue[value] != 0) {
+            currentValue = value
+            clueDialogState = ClueDialogState.MAIN
+        } else {
+            clueDialogState = ClueDialogState.NONE
+        }
     }
 
     fun onRoundDialogDismiss() {
@@ -61,6 +77,8 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel
     }
 
     fun onClueDialogDismiss() {
+        // For tracking internal state.
+        isDailyDouble = false
         clueDialogState = ClueDialogState.NONE
     }
 
@@ -70,26 +88,59 @@ class GameScreenViewModel(gameData: GameData, isResumeGame: Boolean) : ViewModel
 
     fun nextRound() {
         round++
+        // Refresh number of Daily Doubles
+        remainingDailyDoubles = multipliers[round]
         // map returns a List rather than an IntArray. Must convert.
         moneyValues = baseMoneyValues.map { it * multipliers[round] }.toIntArray()
+        // Refresh columnsPerValue
+        columnsPerValue = moneyValues.associateWith { columns }.toMutableMap()
         isShowRoundDialog = false
     }
 
     fun onCorrectResponse(value: Int) {
         score += value
+        if (isDailyDouble) {
+            remainingDailyDoubles--
+            // I feel like there must be an easier way of doing this but this is all I can seem to
+            // get to work.
+            if (columnsPerValue[dailyDoubleInitialValue] != null) {
+                columnsPerValue[dailyDoubleInitialValue] =
+                    columnsPerValue[dailyDoubleInitialValue]!! - 1
+            }
+        } else {
+            if (columnsPerValue[value] != null) {
+                columnsPerValue[value] = columnsPerValue[value]!! - 1
+            }
+        }
         onClueDialogDismiss()
     }
 
     fun onIncorrectResponse(value: Int) {
         score -= value
+        if (isDailyDouble) {
+            remainingDailyDoubles--
+            if (columnsPerValue[dailyDoubleInitialValue] != null) {
+                columnsPerValue[dailyDoubleInitialValue] =
+                    columnsPerValue[dailyDoubleInitialValue]!! - 1
+            }
+        } else {
+            if (columnsPerValue[value] != null) {
+                columnsPerValue[value] = columnsPerValue[value]!! - 1
+            }
+        }
         onClueDialogDismiss()
     }
 
     fun onPass(value: Int) {
+        if (columnsPerValue[value] != null) {
+            columnsPerValue[value] = columnsPerValue[value]!! - 1
+        }
         onClueDialogDismiss()
     }
 
     fun onDailyDouble() {
+        isDailyDouble = true
+        dailyDoubleInitialValue = currentValue
         clueDialogState = ClueDialogState.DAILY_DOUBLE_WAGER
     }
 
