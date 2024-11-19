b45f92ebd3b34452cd3adf589d3e8a39b44f8442
Trevor Martin <tcmartin5000@gmail.com>
2024-11-19 03:54:19 -0500

    More easily manage string resources and dialog state simultaneously.

---
+++ app/src/main/java/com/guillotine/jscorekeeper/data/ClueDialogOptions.kt
@@ -0,0 +1,10 @@
+package com.guillotine.jscorekeeper.data
+
+import com.guillotine.jscorekeeper.R
+
+enum class ClueDialogOptionStringIDs(val stringResourceID: Int) {
+    CORRECT(R.string.correct),
+    INCORRECT(R.string.incorrect),
+    PASS(R.string.pass),
+    DAILY_DOUBLE(R.string.daily_double)
+}
