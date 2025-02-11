d8d3b48fd541b446cf0a9cb7c777178dc625106a
Trevor Martin <tcmartin5000@gmail.com>
2025-02-11 04:35:56 -0500

    Allow scroll for smaller screen devices.

--- app/src/main/java/com/guillotine/jscorekeeper/composable/menu/MenuScreenComposable.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/composable/menu/MenuScreenComposable.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -67,6 +69,7 @@ fun MenuScreenComposable(
             Column(
                 modifier = Modifier
                     .fillMaxHeight()
+                    .verticalScroll(rememberScrollState())
                     // Make the column only as wide as the widest button.
                     .width(IntrinsicSize.Max),
                 verticalArrangement = Arrangement.Center,
