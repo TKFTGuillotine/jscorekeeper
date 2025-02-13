65cde7266f868ac9475cf5dc66445f536c04afb3
Trevor Martin <tcmartin5000@gmail.com>
2025-02-13 01:20:34 -0500

    Vertical just always works better in this case so I've condensed it into one composable. Also fixed insets bug.

--- app/src/main/java/com/guillotine/jscorekeeper/composable/finalj/FinalScreenComposable.kt
+++ app/src/main/java/com/guillotine/jscorekeeper/composable/finalj/FinalScreenComposable.kt
@@ -1,21 +1,46 @@
 package com.guillotine.jscorekeeper.composable.finalj
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.window.core.layout.WindowSizeClass
 import com.guillotine.jscorekeeper.FinalScreen
+import com.guillotine.jscorekeeper.MenuScreen
 import com.guillotine.jscorekeeper.R
+import com.guillotine.jscorekeeper.ResultsScreen
+import com.guillotine.jscorekeeper.composable.general.RadioButtonList
+import com.guillotine.jscorekeeper.composable.general.ScoreCardComposable
+import com.guillotine.jscorekeeper.composable.general.WagerFieldComposable
+import com.guillotine.jscorekeeper.data.ClueTypeRadioButtonOptions
 import com.guillotine.jscorekeeper.viewmodels.FinalScreenViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,45 +72,154 @@ fun FinalScreenComposable(
         },
         modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
-        when (LocalConfiguration.current.orientation) {
-            Configuration.ORIENTATION_LANDSCAPE -> {
-                FinalHorizontalComposable(
-                    innerPadding = innerPadding,
-                    score = viewModel.score,
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .consumeWindowInsets(innerPadding)
+                .windowInsetsPadding(WindowInsets.displayCutout)
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Column(modifier = Modifier.padding(bottom = 16.dp)) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        style = MaterialTheme.typography.titleLarge,
+                        text = stringResource(R.string.entering_final_your_score_is)
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ScoreCardComposable(
+                        viewModel.currency,
+                        viewModel.score,
+                        true,
+                        Modifier.fillMaxWidth()
+                    )
+                }
+            }
+            HorizontalDivider()
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.padding(bottom = 16.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, bottom = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        style = MaterialTheme.typography.titleLarge,
+                        text = stringResource(R.string.final_wager_instructions),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    WagerFieldComposable(
+                        wagerText = viewModel.wagerText,
+                        setWagerText = { viewModel.wagerText = it },
+                        isShowError = viewModel.isShowError,
+                        currency = viewModel.currency,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButtonList(
                     currentSelectedOption = viewModel.currentSelectedRadioButton,
                     onOptionSelected = { viewModel.onRadioButtonSelected(it) },
-                    wagerText = viewModel.wagerText,
-                    setWagerText = { viewModel.wagerText = it },
-                    isShowError = viewModel.isShowError,
-                    submitFinalWager = viewModel::submitFinalWager,
-                    currency = viewModel.currency,
-                    moneyValues = route.moneyValues,
-                    multipliers = route.multipliers,
-                    columns = route.columns,
-                    navController = navController,
-                    showError = viewModel::showError
+                    listOfOptions = listOf(
+                        ClueTypeRadioButtonOptions.CORRECT,
+                        ClueTypeRadioButtonOptions.INCORRECT
+                    ),
+                    // Since parent is scrollable when overflow, this must not be to compile.
+                    scrollable = false
                 )
             }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = {
+                        if (viewModel.wagerText.isNotEmpty()) {
+                            val timestamp = viewModel.submitFinalWager(
+                                viewModel.wagerText.toInt(),
+                                viewModel.score,
+                                viewModel.currentSelectedRadioButton == ClueTypeRadioButtonOptions.CORRECT
+                            )
 
-            else -> {
-                FinalVerticalComposable(
-                    innerPadding = innerPadding,
-                    score = viewModel.score,
-                    currentSelectedOption = viewModel.currentSelectedRadioButton,
-                    onOptionSelected = { viewModel.onRadioButtonSelected(it) },
-                    wagerText = viewModel.wagerText,
-                    setWagerText = { viewModel.wagerText = it },
-                    isShowError = viewModel.isShowError,
-                    submitFinalWager = viewModel::submitFinalWager,
-                    currency = viewModel.currency,
-                    moneyValues = route.moneyValues,
-                    multipliers = route.multipliers,
-                    columns = route.columns,
-                    navController = navController,
-                    showError = viewModel::showError
-                )
+                            if (timestamp != null) {
+                                navController.navigate(
+                                    route = ResultsScreen(
+                                        timestamp = timestamp,
+                                        score = if (viewModel.currentSelectedRadioButton == ClueTypeRadioButtonOptions.CORRECT) {
+                                            viewModel.score + viewModel.wagerText.toInt()
+                                        } else {
+                                            viewModel.score - viewModel.wagerText.toInt()
+                                        },
+                                        moneyValues = route.moneyValues,
+                                        multipliers = route.multipliers,
+                                        currency = route.currency,
+                                        columns = route.columns,
+                                        deleteCurrentSavedGame = true
+                                    )
+                                ) {
+                                    popUpTo(MenuScreen) {
+                                        inclusive = false
+                                    }
+                                }
+                            }
+                        } else {
+                            viewModel.showError()
+                        }
+                    }
+                ) {
+                    Text(stringResource(R.string.submit))
+                }
             }
-
         }
+        /*FinalVerticalComposable(
+            innerPadding = innerPadding,
+            score = viewModel.score,
+            currentSelectedOption = viewModel.currentSelectedRadioButton,
+            onOptionSelected = { viewModel.onRadioButtonSelected(it) },
+            wagerText = viewModel.wagerText,
+            setWagerText = { viewModel.wagerText = it },
+            isShowError = viewModel.isShowError,
+            submitFinalWager = viewModel::submitFinalWager,
+            currency = viewModel.currency,
+            moneyValues = route.moneyValues,
+            multipliers = route.multipliers,
+            columns = route.columns,
+            navController = navController,
+            showError = viewModel::showError
+        )*/
     }
 }
